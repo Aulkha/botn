fcc6c6e73e791d08b8d159b05f3d59072f0368e3
Rakha030806 <rakha.tblt@gmail.com>
2022-02-10 14:37:00 +0700

    Fixed Error Codes, Added signout request

--- botn-backend/routes/auth.js
+++ botn-backend/routes/auth.js
@@ -1,5 +1,5 @@
 // Import Firebase
-import { signInWithEmailAndPassword, setPersistence, browserSessionPersistence } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
+import { signInWithEmailAndPassword, setPersistence, browserSessionPersistence, signOut } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
 
 const authRoute = (router, auth) => {
     
@@ -9,16 +9,26 @@ const authRoute = (router, auth) => {
         const req = await reqBody.value;
         const type = await reqBody.type;
         console.log("1");
-        ctx.assert(type === "json" && req.email && req.password, 400);
+        ctx.assert(type === "json" && req.email && req.password, 403);
         console.log("2");
         console.log(req.email, req.password);
 
         await setPersistence(auth, browserSessionPersistence)
-        const certs = await signInWithEmailAndPassword(auth, req.email, req.password).catch((error) => console.log(error));
+        const certs = await signInWithEmailAndPassword(auth, req.email, req.password).catch((error) => {
+            console.error(error);
+            ctx.throw(400);
+        });
         ctx.response.body = {
             "success": true,
             "certs": certs
         };
+    })
+    router.post("/signout", async (ctx) => {
+        await signOut(auth).catch((error) => {
+            console.error(error);
+            ctx.throw(400);
+        });
+        ctx.response.body = "Signout Successful";
     })
 
 }
