57d1475e58e4b4973defd1301f9d8a6986e6e3f3
Rakha030806 <rakha.tblt@gmail.com>
2022-02-10 17:40:21 +0700

    Added more requests in territory.js

--- botn-backend/routes/territory.js
+++ botn-backend/routes/territory.js
@@ -1,20 +1,67 @@
 // Import Firebase
-import { getDocs, collection } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js";
+import { getDocs, collection, doc, getDoc, addDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js";
+
+const error = (err, ctx) => {
+    console.error(err);
+    ctx.response.status = 400;
+    ctx.response.body = { "message": err };
+}
 
 const territory = (router, db) => {
 
+    router.post("/", async (ctx) => {
+        const reqBody = await ctx.body();
+        const req = await reqBody.value;
+        ctx.assert(reqBody.type === "json", 400);
+
+        try {
+            const addedDoc = await addDoc(await collection(db, "territory"), {
+                name: req.name,
+                occupant: req.occupant,
+                gameLink: req.gameLink,
+                currentBattle: null
+            })
+
+            ctx.response.body = { addedDoc };
+        } catch (err){
+            error(err, ctx);
+        }
+    })
+
     router.get("/map", async (ctx) => {
-        const querySnapshot = await getDocs(collection(db, "territory"));
+        const querySnapshot = await getDocs(await collection(db, "territory"));
         const response = {};
-        querySnapshot.forEach((doc) => {
-            const docData = doc.data();
-            response[doc.id] = {
+        querySnapshot.forEach((document) => {
+            const docData = document.data();
+            response[document.id] = {
                 "name": docData.name,
                 "occupant": docData.occupant
             };
         });
         console.log(response);
         ctx.response.body = response;
+    })
+
+    router.get("/:id", async (ctx) => {
+        const querySnapshot = await getDoc(await doc(db, "territory", ctx?.params?.id));
+        const document = querySnapshot.data();
+        if (document.currentBattle !== null) {
+            document.currentBattle = document.currentBattle.data();
+        }
+
+        ctx.response.body = document;
+    })
+    router.patch("/:id", async (ctx) => {
+        const reqBody = await ctx.body();
+        const req = await reqBody.value;
+        ctx.assert(reqBody.type === "json", 400);
+
+        try {
+            const updatedDoc = await updateDoc(await doc(db, "territory", ctx?.params?.id), req)
+            ctx.response.body = { updatedDoc };
+        } catch (err){
+            error(err, ctx);
+        }
     })
 
 };
