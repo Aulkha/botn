c3cc50aa237b383dcfd0bcd648d8670dee11fedc
Rakha030806 <rakha.tblt@gmail.com>
2022-02-10 14:21:15 +0700

    Update auth.js

--- botn-backend/routes/auth.js
+++ botn-backend/routes/auth.js
@@ -1,5 +1,5 @@
 // Import Firebase
-import { signInWithEmailAndPassword, setPersistence, inMemoryPersistence } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
+import { signInWithEmailAndPassword, setPersistence, browserSessionPersistence } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
 
 const authRoute = (router, auth) => {
     
@@ -13,7 +13,7 @@ const authRoute = (router, auth) => {
         console.log("2");
         console.log(req.email, req.password);
 
-        await setPersistence(auth, inMemoryPersistence)
+        await setPersistence(auth, browserSessionPersistence)
         const certs = await signInWithEmailAndPassword(auth, req.email, req.password).catch((error) => console.log(error));
         ctx.response.body = {
             "success": true,
